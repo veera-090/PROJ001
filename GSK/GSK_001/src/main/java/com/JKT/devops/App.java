4c12008f4286ac546ed8ab8924e783580255fdbe
Veera-090 <veera.mohan090@gmail.com>
2021-10-15 07:51:03 +0000

    GSK Project

---
+++ GSK/GSK_001/src/main/java/com/JKT/devops/App.java
@@ -0,0 +1,13 @@
+package com.JKT.devops;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
